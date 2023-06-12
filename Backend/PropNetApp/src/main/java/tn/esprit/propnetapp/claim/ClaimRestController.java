2b78226f48523463880a8853342ceb846c10a027
baligh8 <obaligh8@gmail.com>
2023-06-12 22:03:38 +0100

    CRUD Claim and Feedback

--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/claim/ClaimRestController.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/claim/ClaimRestController.java
@@ -2,6 +2,9 @@ package tn.esprit.propnetapp.claim;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.propnetapp.locality.Locality;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -13,4 +16,24 @@ public class ClaimRestController {
     public Claim addClaimS(@RequestBody Claim claim) {
         return claimService.addClaim(claim);
     }
+
+    @GetMapping("/retrieveAllClaim")
+    public List<Claim> getClaims() {
+        List<Claim> listClaims = claimService.retrieveAllClaim();
+        return listClaims;
+    }
+    @GetMapping("/retrieve-claim/{claim-id}")
+    public Claim retrieveClaim(@PathVariable("claim-id")  Integer idClaim) {
+        return claimService.retrieveClaim(idClaim);
+    }
+    @DeleteMapping("/delete-claim/{claim-id}")
+    public void deleteCllaim(@PathVariable("claim-id") Integer idClaim) {
+        claimService.deleteClaim(idClaim);
+    }
+    @PutMapping("/update-claim")
+    public Claim updateClaim(@RequestBody Claim claim) {
+
+        return claimService.updateClaim(claim);
+    }
+
 }
--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/claim/ClaimServiceImpl.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/claim/ClaimServiceImpl.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 @Aspect
@@ -19,4 +21,29 @@ public class ClaimServiceImpl implements IClaimService {
     public Claim addClaim(Claim claim) {
         return claimRepository.save(claim);
     }
+
+    @Override
+    public List<Claim> retrieveAllClaim() {
+        return claimRepository.findAll();
+    }
+
+    @Override
+    public Claim updateClaim(Claim c) {
+
+        return claimRepository.save(c);
+    }
+
+    @Override
+    public Claim retrieveClaim(Integer idClaim) {
+        return claimRepository.findById(idClaim).get();
+    }
+
+    @Override
+    public void deleteClaim(Integer idClaim) {
+        claimRepository.deleteById(idClaim);
+
+    }
+
+
+
 }
