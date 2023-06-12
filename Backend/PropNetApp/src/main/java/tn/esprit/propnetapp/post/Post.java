5845203c2def8cdfbffe1cd829b1f8490a7668ca
aymen <aymen.moulehi@outlook.com>
2023-06-12 20:23:14 +0100

    Adapting the data model to Frontend.

--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/post/Post.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/post/Post.java
@@ -10,6 +10,7 @@ import tn.esprit.propnetapp.response.Response;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
@@ -19,29 +20,16 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post implements Serializable {
-    /**
-     * ID: Long
-     * Title: String
-     * Content: String
-     * Date: Date
-     * Likes: Integer
-     * Comments: Array or List of Comment objects
-     * Tags: Array or List of Strings
-     * Image: String or URL
-     * ShareCount: Integer
-     * Views: Integer
-     * Location: String
-     * Hashtags: Array or List of Strings
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idPost;
     private String title;
     private String Content;
     private Date postDate;
+    private Byte[] image;
     private Integer likes = 0;
     @OneToMany(mappedBy = "post")
-    private Collection<Response> responses;
+    private Collection<Response> responses = new ArrayList<Response>();
     @ManyToOne
     private AppUser appUser;
 }
