41a5932522e9a878bbccbfd8d6ef75c9254399ba
moataztrojette <moataztrojette@gmail.com>
2023-06-13 23:38:36 +0200

    findPostWithLocation + Query

--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/appuser/IAppUserService.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/appuser/IAppUserService.java
@@ -1,5 +1,9 @@
 package tn.esprit.propnetapp.appuser;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IAppUserService {
     AppUser addAppUser(AppUser appUser);
+    List<Map<String, Object>> findPostWithLocation();
 }
