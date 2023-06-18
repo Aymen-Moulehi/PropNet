bbe0dbe828105daf213053502858b3f77739365c
moataztrojette <moataztrojette@gmail.com>
2023-06-19 00:35:07 +0200

    updating v2

--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/post/PostServiceImpl.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/post/PostServiceImpl.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 @Aspect
@@ -24,4 +26,34 @@ public class PostServiceImpl implements IPostService {
     public Post getPostById(Integer postId) {
         return postRepository.findPostByIdPost(postId);
     }
+
+    @Override
+    public List<Post> getPostAccepted() {
+        List<Post> acceptedPosts = postRepository.findByStatus("Accepted");
+        return acceptedPosts;
+
+    }
+
+    @Override
+    public List<Post> getPostPendding() {
+        List<Post> PendingPosts = postRepository.findByStatus("Pending");
+        return PendingPosts;    }
+
+    @Override
+    public List<Post> getPost() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public void deletePost(Integer id) {
+         postRepository.deleteById(id);
+    }
+
+    @Override
+    public void updatePost(Integer id) {
+        Post _post = postRepository.findPostByIdPost(id);
+        _post.setStatus("Accepted");
+    }
+
+
 }
