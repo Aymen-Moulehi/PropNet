8ae1a214ec314dd38d12c2b5f5b0cc2c283bfe25
moataztrojette <moataztrojette@gmail.com>
2023-06-25 22:08:31 +0200

    v4

---
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/features/email/EmailDetail.java
@@ -0,0 +1,27 @@
+package tn.esprit.propnetapp.features.email;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetail implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEmailDetail;
+    private String recipient;
+    @Column(length = 5000)
+    private String msgBody;
+    private String subject;
+    private Date creationDate = new Date();
+}
---
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/features/email/EmailDetailRepository.java
@@ -0,0 +1,7 @@
+package tn.esprit.propnetapp.features.email;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailDetailRepository extends JpaRepository<EmailDetail, Integer> {
+
+}
