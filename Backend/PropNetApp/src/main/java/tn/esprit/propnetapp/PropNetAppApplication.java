81f79290a587bb1fd3831d04a813b586ce015397
moataztrojette <moataztrojette@gmail.com>
2023-07-02 20:06:44 +0200

    send email to users with special offer (batch)

--- Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/PropNetAppApplication.java
+++ Backend/PropNetApp/src/main/java/tn/esprit/propnetapp/PropNetAppApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.propnetapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PropNetAppApplication {
 
     public static void main(String[] args) {
